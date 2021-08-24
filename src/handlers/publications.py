402f2b7425391ef4fe6f224c92e69a7521d49f04
rosselton <rebecca.osselton@ncl.ac.uk>
2021-08-24 15:49:38 +0100

    Writes all file locally

--- src/handlers/publications.py
+++ src/handlers/publications.py
@@ -8,35 +8,40 @@ import time
 from scholarly import scholarly
 import azure.functions as func
 
-
-author_list = ['GjSCPJ0AAAAJ', '_NdzLicAAAAJ', 'ujbju80AAAAJ', 'z6jaMRcAAAAJ', 'NVBSg9gAAAAJ', 'oAjiq3QAAAAJ', '5ydGTN0AAAAJ', 'UeMGAaAAAAAJ', 'WqH63QAAAAJ', 'dH4219oAAAAJ', '2Kcd0zoAAAAJ', '4zjd8H8AAAAJ', 'akdJAsEAAAAJ'] 
+# WqH63QAAAAJ Ellen Moss dodgy?
+author_list = ['UeMGAaAAAAAJ','dH4219oAAAAJ','2Kcd0zoAAAAJ', '4zjd8H8AAAAJ','akdJAsEAAAAJ','GjSCPJ0AAAAJ', '_NdzLicAAAAJ', 'ujbju80AAAAJ', 'z6jaMRcAAAAJ', 'NVBSg9gAAAAJ', 'oAjiq3QAAAAJ', '5ydGTN0AAAAJ'] 
 
 def isAvailable(publication, pub_attribute):
     try:
         publication[pub_attribute]
-        publication = publication[pub_attribute]
-        return publication
+        return publication[pub_attribute]
     except:
         return "Unavailable" 
 
 def isAvailableInBib(publication, pub_attribute):
     try:
         publication['bib'][pub_attribute]
-        publication = publication['bib'][pub_attribute]
-        return publication
+        return publication['bib'][pub_attribute]
     except:
         return "Unavailable" 
 
+def inAuthor(author, attribute):
+    try:
+        author[attribute]
+        return author[attribute]
+    except:
+        return "Unavailable" 
 
+    
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    local_path = "./data"
+    #local_path = "./data"
 
     # make directory if it doesn't exist
-    if not os.path.exists(local_path):
-        os.makedirs(local_path)
+    #if not os.path.exists(local_path):
+        #os.makedirs(local_path)
 
     # loop through the author list
     for each_author in author_list:
@@ -64,89 +69,81 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 #except: # catch all exceptions
                     #e = sys.exc_info()[0]
                     #logging.error(f' error doing query{e}')
-
-            a_name = author['name']
-            a_affiliation = author['affiliation'] 
-            a_authorID = author['scholar_id']
-            a_publications = author['publications']
+                    
+        
+            a_name = inAuthor(author, 'name')
+            a_affiliation = inAuthor(author, 'affiliation')
+            a_authorID = inAuthor(author, 'scholar_id')
+            a_publications = inAuthor(author, 'publications')
 
             pubs_list = []
-            title = ""
-            author_pub_id = ""
-            num_citations = ""
-            pub_year = ""
-            pub_url = ""
-            author = ""
-            journal = ""
-            abstract = ""
-            volume = ""
-            number = ""
-            pages = ""
-            publisher = ""
-
-            # loop through publications creating a dict each time, then add to pubs_list 
-            for publication in a_publications:
-
-                title = isAvailableInBib(publication, 'title')    
-                author_pub_id = isAvailable(publication, 'author_pub_id')
-                num_citations = isAvailable(publication, 'num_citations')
-                pub_url = isAvailable(publication, 'pub_url')
-                pub_year = isAvailableInBib(publication, 'pub_year')
-                author = isAvailableInBib(publication, 'author')
-                journal = isAvailableInBib(publication, 'journal')
-                volume = isAvailableInBib(publication, 'volume')
-                number = isAvailableInBib(publication, 'number')
-                pages = isAvailableInBib(publication, 'pages')
-                publisher = isAvailableInBib(publication, 'publisher')
-                abstract = isAvailableInBib(publication, 'abstract')
-
-                pub_dict = {
-                    "title" : title,
-                    "pub_year" : pub_year,
-                    "author_pub_id" : author_pub_id,
-                    "num_citations" : num_citations,
-                    "pub_url" : pub_url, 
-                    "author" : author,
-                    "journal" : journal,
-                    "volume" : volume,
-                    "number" : number,
-                    "pages" : pages,
-                    "publisher" : publisher,
-                    "abstract" : abstract
-                }
-                pubs_list.append(pub_dict.copy())
-
-                
-            # create another dict including the pubs_list
-            author_dict = {
-                "name" : a_name,
-                "affiliation" : a_affiliation,
-                "authorID" : a_authorID,
-                "publications" : pubs_list
-            }
-
-        
-            # create suitable author filename
-            filename = a_name.replace(" ", "_").lower()
-            authorfile = open("data/" + filename + "_" + a_authorID + ".json", "a") 
-
+           
             try:
-                authorfile.write(json.dumps(author_dict))
+                a_publications
+
+                # loop through publications creating a dict each time, then add to pubs_list 
+                for publication in a_publications:
+
+                    title = isAvailableInBib(publication, 'title')    
+                    author_pub_id = isAvailable(publication, 'author_pub_id')
+                    num_citations = isAvailable(publication, 'num_citations')
+                    pub_url = isAvailable(publication, 'pub_url')
+                    pub_year = isAvailableInBib(publication, 'pub_year')
+                    author = isAvailableInBib(publication, 'author')
+                    journal = isAvailableInBib(publication, 'journal')
+                    volume = isAvailableInBib(publication, 'volume')
+                    number = isAvailableInBib(publication, 'number')
+                    pages = isAvailableInBib(publication, 'pages')
+                    publisher = isAvailableInBib(publication, 'publisher')
+                    abstract = isAvailableInBib(publication, 'abstract')
+
+                    pub_dict = {
+                        "title" : title,
+                        "pub_year" : pub_year,
+                        "author_pub_id" : author_pub_id,
+                        "num_citations" : num_citations,
+                        "pub_url" : pub_url, 
+                        "author" : author,
+                        "journal" : journal,
+                        "volume" : volume,
+                        "number" : number,
+                        "pages" : pages,
+                        "publisher" : publisher,
+                        "abstract" : abstract
+                    }
+                    pubs_list.append(pub_dict.copy())
+
+                    
+                # create another dict including the pubs_list
+                author_dict = {
+                    "name" : a_name,
+                    "affiliation" : a_affiliation,
+                    "authorID" : a_authorID,
+                    "publications" : pubs_list
+                }
+
             
-            except: 
-                e = sys.exc_info()[0]
-                logging.error(f' error writing to file{e}')
+                # create suitable author filename
+                filename = a_name.replace(" ", "_").lower()
+                #authorfile = open("data/" + filename + "_" + a_authorID + ".json", "a") 
+                authorfile = open(filename + "_" + a_authorID + ".json", "a") 
 
-            finally:
-                authorfile.close()
+                try:
+                    authorfile.write(json.dumps(author_dict))
+                
+                except: 
+                    e = sys.exc_info()[0]
+                    logging.error(f' error writing to file{e}')
 
-            #return func.HttpResponse(jsonpickle.encode(filename))
-       # else:
-           # return func.HttpResponse(
-              #  "Please pass a authorID on the query string or in the request body",
-              #  status_code=400
-          #  )
-    time.sleep(2)
+                finally:
+                    authorfile.close()
+
+            except:
+                 e = sys.exc_info()[0]
+                 logging.error(f' No publications{e}')
+
+       
+    time.sleep(1)
 
     return func.HttpResponse( 
             "All authors complete",
